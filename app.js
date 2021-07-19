1622daf4f1f7f2486b4b445e0074d526c6c922e7
steven4program <stevenchang421@gmail.com>
2021-07-20 00:04:27 +0800

    Use JSON and #each to render page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // include packages and define server related variables
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const jobs = require('./job.json')
 const app = express()
 const port = 3000
 
@@ -8,9 +10,11 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { job: jobs.results })
 })
 
 app.listen(port, () => {
